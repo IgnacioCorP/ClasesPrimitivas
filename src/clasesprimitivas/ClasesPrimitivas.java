166180b2c1f51c94531cbcc31e2dec2cf414b361
Ignacio Correa <nachitoficopal@gmail.com>
2022-09-27 09:49:40 +0200

    Clases Primitivas proyecto java

---
+++ src/clasesprimitivas/ClasesPrimitivas.java
@@ -0,0 +1,39 @@
+package clasesprimitivas;
+
+
+
+
+public class ClasesPrimitivas {
+    
+/*
+  * Enteros:
+        - byte: 8 bits(-128 a 127) | Byte.MIN_VALUE - MAX_VALUE
+        - short: 16 bits (-32768 a 32767) | Short.MIN_VALUE - MAX_VALUE
+        - int:
+        - long;
+    
+  * Decimales:
+        - Float:
+        - Double:
+    
+   * Decimales:
+        - Char:
+    
+*/
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+  
+        
+
+        System.out.println("Valor máximo del tipo Byte "+Byte.MAX_VALUE+" y Valor mínimo del tipo Byte es "+Byte.MIN_VALUE+"\n");
+        System.out.println("Valor máximo del tipo Short "+Short.MAX_VALUE+" y Valor mínimo del tipo Short es "+Short.MIN_VALUE+"\n");
+        System.out.println("Valor máximo del tipo Int "+Integer.MAX_VALUE+" y Valor mínimo del tipo Int es "+Integer.MIN_VALUE+"\n");
+        System.out.println("Valor máximo del tipo Long "+Long.MAX_VALUE+" y Valor mínimo del tipo Long es "+Long.MIN_VALUE+"\n");      
+        System.out.println("Valor máximo del tipo Float "+Float.MAX_VALUE+" y Valor mínimo del tipo Float es "+Float.MIN_VALUE+"\n");
+        System.out.println("Valor máximo del tipo Double "+Double.MAX_VALUE+" y Valor mínimo del tipo Double es "+Double.MIN_VALUE+"\n");
+       // System.out.println("Valor máximo del tipo Long "+Char.MAX_VALUE+" y Valor mínimo del tipo Long es "+Char.MIN_VALUE+"\n");
+    }
+    
+}
+
